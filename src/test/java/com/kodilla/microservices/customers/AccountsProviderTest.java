3e589e054adfca01db827666e9007bb27d3de1f1
kacper <kacper.szubra>
2020-10-13 19:05:24 +0200

    lesson

--- src/test/java/com/kodilla/microservices/customers/AccountsProviderTest.java
+++ src/test/java/com/kodilla/microservices/customers/AccountsProviderTest.java
@@ -1,2 +1,41 @@
-package com.kodilla.microservices.customers;public class AccountsProviderTest {
+package com.kodilla.microservices.customers;
+
+import com.kodilla.microservices.customers.api.snapshot.AccountSnapshot;
+import com.kodilla.microservices.customers.service.interfaces.providers.CustomerAccountsProvider;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@AutoConfigureStubRunner(
+        stubsMode = StubRunnerProperties.StubsMode.LOCAL,
+        ids = {
+                "com.kodilla:accounts:+:stubs:9000"
+        }
+)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class AccountsProviderTest {
+    @Autowired
+    private CustomerAccountsProvider accountsProvider;
+
+    @Test
+    void findCustomerAccounts() {
+        //Given
+        Long customerId = 1L;
+
+        //When
+        List<AccountSnapshot> accounts = accountsProvider.getCustomerAccounts(customerId);
+
+        //Then
+        assertEquals(1, accounts.size());
+    }
+
 }
